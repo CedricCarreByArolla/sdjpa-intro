4e49da4179d349e33b0f970cc68282a21111aa05
Cédric Carré <cedric.carre@arolla.fr>
2022-12-16 11:26:19 +0100

    add transaction test using @Order annotation and @Commit Vs @RollBack(false)

--- src/test/java/guru/springframework/sdjpaintro/TestJpaTestSplice.java
+++ src/test/java/guru/springframework/sdjpaintro/TestJpaTestSplice.java
@@ -2,18 +2,25 @@ package guru.springframework.sdjpaintro;
 
 import guru.springframework.sdjpaintro.domain.Book;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.core.annotation.Order;
+import org.springframework.test.annotation.Commit;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 public class TestJpaTestSplice {
 
     @Autowired
     BookRepository bookRepository;
 
+    @Commit
+    @Order(1)
     @Test
     void testJpaTestSplice() {
         long countBefore = bookRepository.count();
@@ -23,5 +30,13 @@ public class TestJpaTestSplice {
         long countAfter = bookRepository.count();
 
         assertThat(countBefore).isLessThan(countAfter);
+    }
+
+    @Order(2)
+    @Test
+    void testJpaTestSpliceTransaction() {
+        long countBefore = bookRepository.count();
+
+        assertThat(countBefore).isEqualTo(1);
     }
 }
