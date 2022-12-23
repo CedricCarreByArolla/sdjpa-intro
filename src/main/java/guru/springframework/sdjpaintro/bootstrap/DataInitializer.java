6ae4dabdd8a6011a7a60e95655478dc2d07b116a
Cédric Carré <cedric.carre@arolla.fr>
2022-12-23 13:21:29 +0100

    add @Profile({local, default}) and deleteAll in DataIntializer.run to reset BDD at each new run

--- src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
@@ -3,8 +3,10 @@ package guru.springframework.sdjpaintro.bootstrap;
 import guru.springframework.sdjpaintro.domain.Book;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile({"local", "default"})
 @Component
 public class DataInitializer implements CommandLineRunner {
 
@@ -16,17 +18,13 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        bookRepository.deleteAll();
+
         Book bookBDD = new Book("Behavior Driven Design", "987", "Arolla Edition");
-        Book bookTDD = new Book("Crafters", "654", "Square Edition");
-
-        System.out.println("Id : " + bookBDD.getId());
-        System.out.println("Id : " + bookTDD.getId());
-
         Book savedBDD = bookRepository.save(bookBDD);
-        Book savedTDD = bookRepository.save(bookTDD);
 
-        System.out.println("Id : " + savedBDD.getId());
-        System.out.println("Id : " + savedTDD.getId());
+        Book bookTDD = new Book("Crafters", "654", "Square Edition");
+        Book savedTDD = bookRepository.save(bookTDD);
 
         bookRepository.findAll().forEach(book -> {
             System.out.println("Book Id : " + book.getId());
